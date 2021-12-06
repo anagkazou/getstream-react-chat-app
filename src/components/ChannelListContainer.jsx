6be01c606f6ae29363509a26500b95d26f3d4324
davidomole <thedavidomole@gmail.com>
2021-12-06 15:17:16 +0100

    create teamchannellist and teamchannelpreview components

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -34,6 +34,24 @@ const ChannelListContainer = () => {
             <div className="channel-list__list__wrapper">
                 <CompanyHeader/>
                 <ChannelSearch/>
+                <ChannelList filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList {...listProps} type="team"/>
+                    )}
+                    Preview={(previewProps) =>(
+                        <TeamChannelPreview {...previewProps } type="team"/>
+                    ) }
+                /> 
+                <ChannelList filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList {...listProps} type="messaging"/>
+                    )}
+                    Preview={(previewProps) =>(
+                        <TeamChannelPreview {...previewProps } type="messaging"/>
+                    ) }
+                /> 
             </div>
         </>
     )
