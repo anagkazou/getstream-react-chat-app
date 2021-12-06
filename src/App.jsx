8f362ee677f3e6ba7eaef9915f6c9d779b559e60
davidomole <thedavidomole@gmail.com>
2021-12-06 15:25:32 +0100

    🚧 create Auth component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie/es6";
 
-import { ChannelListContainer, ChannelContainer } from "./components";
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 
 import './App.css'
 
@@ -11,7 +11,12 @@ import './App.css'
 const apiKey = "v5r44zjnwr2y";
 
 const client = StreamChat.getInstance(apiKey)
+
+const authToken = false;
+
 const App = () => {
+
+  if (!authToken) return <Auth/>
   return (
     <div className="app__wrapper">
       <Chat client ={client} theme ="team dark">
---
+++ src/components/Auth.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import Cookies from 'universal-cookie/es6'
+import axios from 'axios';
+const Auth = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Auth
